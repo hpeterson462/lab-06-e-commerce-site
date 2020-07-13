f12a2bc3091fa68cbabefead4351751aa5308dd9
Heather Peterson <hpeterson462@gmail.com>
2020-07-13 13:57:10 -0700

    initial commit

---
+++ data/flowers.js
@@ -0,0 +1,63 @@
+const iris = {
+    id: 'iris',
+    name: 'Bearded Iris',
+    image: 'iris.png',
+    description: 'A rainbow of showy flowers.',
+    category: 'spring',
+    price: 2.00
+};
+
+const rose = {
+    id: 'rose',
+    name: 'Climbing Rose',
+    image: 'rose.png',
+    description: 'A trellis of colors.',
+    category: 'summer',
+    price: 3.00
+};
+
+const daffodil = {
+    id: 'daffodil',
+    name: 'Trumpet Daffodil',
+    image: 'daffodil.png',
+    description: 'Trumpeting in spring.',
+    category: 'spring',
+    price: 1.00
+};
+
+const sunflower = {
+    id: 'sunflower',
+    name: 'Skyscraper Sunflower',
+    image: 'sunflower.png',
+    description: 'Scraping the summer skies.',
+    category: 'summer',
+    price: 3.00
+};
+
+const daisy = {
+    id: 'daisy',
+    name: 'English Daisy',
+    image: 'daisy.png',
+    description: 'Summer blooms in an English garden.',
+    category: 'summer',
+    price: 1.00
+};
+
+const peony = {
+    id: 'peony',
+    name: 'Bowl of Beauty Peony',
+    image: 'peony.png',
+    description: 'Bowls of pink beauty.',
+    category: 'spring',
+    price: 5.00
+};
+
+const Dahlia = {
+    id: 'dahlia',
+    name: 'Waterlily Dahlia',
+    image: 'dahlia.png',
+    description: 'Bursting in summer bloom.',
+    category: 'summer',
+    price: 3.00
+};
+
